889fa9542b2c4dd3a53b68e08426deb7e046eee1
Loyal Bajwa <singhloyaljot2778@gmail.com>
2022-07-12 22:53:56 -0400

    Update views

--- home/views.py
+++ home/views.py
@@ -1,20 +1,9 @@
-from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from django.views.decorators.cache import never_cache
-from django.http.response import HttpResponse, HttpResponseRedirect
-from django.core.handlers.wsgi import WSGIRequest
-from dataclasses import dataclass
-from typing import Union
+from django.shortcuts import redirect
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Member, MemberForm
 
-
-@dataclass
-class OperationsHTML:
-    heading: str
-    sub_heading: str
-    button_name: str
-    is_edit: bool = False
-    is_add: bool = False
+template_name = "operations.html"
 
 
 class HomeList(ListView):
@@ -22,30 +11,23 @@ class HomeList(ListView):
     template_name = "home.html"
 
 
-def add_member(request: WSGIRequest) -> Union[HttpResponseRedirect, HttpResponse]:
-    form = MemberForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('/')
-    spec = OperationsHTML(button_name='Add', heading='Add a team member', sub_heading='Set name, contact info and role',
-                          is_add=True)
-    context = {'form': form, 'spec': spec}
-    return render(request, 'operations.html', context)
+class AddMemberView(CreateView):
+    model = Member
+    template_name = template_name
+    extra_context = {'operation': 'Add'}
+    form_class = MemberForm
+    success_url = '/'
 
 
-@never_cache
-def edit_member(request: WSGIRequest, member_id: str) -> Union[HttpResponseRedirect, HttpResponse]:
-    if not Member.objects.filter(pk=member_id):
-        return redirect('/')
-    member = Member.objects.get(pk=member_id)
-    if 'delete' in request.POST:
-        member.delete()
-        return redirect('/')
-    form = MemberForm(request.POST or None, instance=member)
-    if form.is_valid():
-        form.save()
-        return redirect('/')
-    spec = OperationsHTML(button_name='Edit', heading='Edit team member',
-                          sub_heading='Edit name, contact info and role', is_edit=True)
-    context = {'form': form, 'spec': spec}
-    return render(request, 'operations.html', context)
+class EditMemberView(UpdateView):
+    model = Member
+    template_name = template_name
+    extra_context = {'operation': 'Edit'}
+    form_class = MemberForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        if 'delete' in request.POST:
+            Member.objects.get(pk=self.kwargs.get('pk')).delete()
+            return redirect('/')
+        return super().post(request, *args, **kwargs)
